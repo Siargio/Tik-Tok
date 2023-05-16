59441f8dc8fb90f7e040068586f3df3a5b95b2c6
Siarheimasiuk <winst73@gmail.com>
2023-05-16 12:07:41 +0300

    add scrollView

---
+++ Tik-Tok/Model/Model.swift
@@ -0,0 +1,22 @@
+//
+//  Model.swift
+//  Tik-Tok
+//
+//  Created by Sergio on 16.05.23.
+//
+
+import Foundation
+
+struct PostModel {
+
+    let identifier: String
+
+    static func mockModels() -> [PostModel] {
+        var posts = [PostModel]()
+        for _ in 0...100 {
+            let post = PostModel(identifier: UUID().uuidString)
+            posts.append(post)
+        }
+        return posts
+    }
+}
